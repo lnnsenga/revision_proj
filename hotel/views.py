4ea1786ac4a0c66f5cde51c00dc8c21294641c5d
lnnsenga <codingbaws@gmail.com>
2024-10-22 11:57:50 +0300

    created empty functions

--- hotel/views.py
+++ hotel/views.py
@@ -1,3 +1,60 @@
 from django.shortcuts import render
+from .models import User
 
-# Create your views here.
+
+
+
+def user_login(request):
+    pass
+
+def homepage(request):
+    pass
+
+def list_room(request):
+    pass
+
+def single_room(request):
+    pass
+
+def book_room(request):
+    pass
+
+def pay_for_booking(request):
+    pass
+
+
+def checkin_room(request):
+    pass
+
+
+def checkout_room(request):
+    pass
+
+
+
+def customer_dashboard(request):
+    pass
+
+def admin_dashboard(request):
+    pass
+
+def list_admin(request):
+    pass
+
+def create_admin(request):
+    pass
+
+def show_admin(request):
+    pass
+
+def edit_admin(request):
+    pass
+
+def delete_admin(request):
+    pass
+
+def logs(request):
+    pass
+
+    
+    
