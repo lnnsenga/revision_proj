e33dccccbf5fe000cd195f1dc3d1982b57be7136
lnnsenga <codingbaws@gmail.com>
2024-10-22 11:56:38 +0300

    registered all models

--- hotel/admin.py
+++ hotel/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import User, Customer, Staff, Room, Payment, Reservation, Booking, PaymentType, RoomStatus, RoomType
 
 # Register your models here.
+admin.site.register([User, Customer, Staff, Room, Payment, Reservation, Booking, PaymentType, RoomStatus, RoomType])
